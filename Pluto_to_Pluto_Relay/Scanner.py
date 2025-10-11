bf935e374ec01149707e57827627cf091b35cb32
Brauuwu <survivalminecraft080@gmail.com>
2025-10-11 22:21:43 +0700

    Pluto_to_Pluto

---
+++ Pluto_to_Pluto_Relay/Scanner.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: sact
+# GNU Radio version: 3.10.11.0
+
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import iio
+import Scanner_epy_block_0 as epy_block_0  # embedded python block
+import Scanner_epy_block_1 as epy_block_1  # embedded python block
+import gnuradio.lora_sdr as lora_sdr
+import numpy as np
+import threading
+
+
+
+
+class Scanner(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        self.flowgraph_started = threading.Event()
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.soft_decoding = soft_decoding = True
+        self.sf = sf = 10
+        self.samp_rate = samp_rate = 500000
+        self.pay_len = pay_len = 11
+        self.impl_head = impl_head = False
+        self.has_crc = has_crc = True
+        self.frame_period = frame_period = 1000
+        self.cr = cr = 2
+        self.center_freq_0 = center_freq_0 = 433000000
+        self.center_freq = center_freq = 868000000
+        self.bw = bw = 125000
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.lora_sdr_whitening_0 = lora_sdr.whitening(False,False,'\n','packet_len')
+        self.lora_sdr_modulate_0 = lora_sdr.modulate(sf, samp_rate, bw, [8,16], (int(20*2**sf*samp_rate/bw)),8)
+        self.lora_sdr_modulate_0.set_min_output_buffer(10000000)
+        self.lora_sdr_interleaver_0 = lora_sdr.interleaver(cr, sf, 0, 125000)
+        self.lora_sdr_header_decoder_0 = lora_sdr.header_decoder(impl_head, cr, pay_len, has_crc, False, True)
+        self.lora_sdr_header_0 = lora_sdr.header(impl_head, has_crc, cr)
+        self.lora_sdr_hamming_enc_0 = lora_sdr.hamming_enc(cr, sf)
+        self.lora_sdr_hamming_dec_0 = lora_sdr.hamming_dec(soft_decoding)
+        self.lora_sdr_gray_mapping_0 = lora_sdr.gray_mapping( soft_decoding)
+        self.lora_sdr_gray_demap_0 = lora_sdr.gray_demap(sf)
+        self.lora_sdr_frame_sync_0 = lora_sdr.frame_sync(int(center_freq), bw, sf, impl_head, [18], (int(samp_rate/bw)),8)
+        self.lora_sdr_fft_demod_0 = lora_sdr.fft_demod( soft_decoding, True)
+        self.lora_sdr_dewhitening_0 = lora_sdr.dewhitening()
+        self.lora_sdr_deinterleaver_0 = lora_sdr.deinterleaver( soft_decoding)
+        self.lora_sdr_crc_verif_0 = lora_sdr.crc_verif( 1, False)
+        self.lora_sdr_add_crc_0 = lora_sdr.add_crc(has_crc)
+        self.iio_pluto_source_0 = iio.fmcomms2_source_fc32('usb:1.5.5' if 'usb:1.5.5' else iio.get_pluto_uri(), [True, True], 32768)
+        self.iio_pluto_source_0.set_len_tag_key('packet_len')
+        self.iio_pluto_source_0.set_frequency(center_freq)
+        self.iio_pluto_source_0.set_samplerate(samp_rate)
+        self.iio_pluto_source_0.set_gain_mode(0, 'slow_attack')
+        self.iio_pluto_source_0.set_gain(0, 64)
+        self.iio_pluto_source_0.set_quadrature(True)
+        self.iio_pluto_source_0.set_rfdc(True)
+        self.iio_pluto_source_0.set_bbdc(True)
+        self.iio_pluto_source_0.set_filter_params('Auto', '', 0, 0)
+        self.iio_pluto_sink_1 = iio.fmcomms2_sink_fc32('usb:1.5.5' if 'usb:1.5.5' else iio.get_pluto_uri(), [True, True], 32768, False)
+        self.iio_pluto_sink_1.set_len_tag_key('')
+        self.iio_pluto_sink_1.set_bandwidth(20000000)
+        self.iio_pluto_sink_1.set_frequency(center_freq_0)
+        self.iio_pluto_sink_1.set_samplerate(samp_rate)
+        self.iio_pluto_sink_1.set_attenuation(0, 10.0)
+        self.iio_pluto_sink_1.set_filter_params('Auto', '', 0, 0)
+        self.epy_block_1 = epy_block_1.mqtt_subscriber(host='127.0.0.1', port=1883, username='root', password='1', topic='data/input', buf_len=1024, max_len=255)
+        self.epy_block_0 = epy_block_0.mqtt_publisher(host='127.0.0.1', port=1883, username='root', password='1', topic='data/output')
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.lora_sdr_header_decoder_0, 'frame_info'), (self.lora_sdr_frame_sync_0, 'frame_info'))
+        self.connect((self.epy_block_1, 0), (self.lora_sdr_whitening_0, 0))
+        self.connect((self.iio_pluto_source_0, 0), (self.lora_sdr_frame_sync_0, 0))
+        self.connect((self.lora_sdr_add_crc_0, 0), (self.lora_sdr_hamming_enc_0, 0))
+        self.connect((self.lora_sdr_crc_verif_0, 0), (self.epy_block_0, 0))
+        self.connect((self.lora_sdr_deinterleaver_0, 0), (self.lora_sdr_hamming_dec_0, 0))
+        self.connect((self.lora_sdr_dewhitening_0, 0), (self.lora_sdr_crc_verif_0, 0))
+        self.connect((self.lora_sdr_fft_demod_0, 0), (self.lora_sdr_gray_mapping_0, 0))
+        self.connect((self.lora_sdr_frame_sync_0, 0), (self.lora_sdr_fft_demod_0, 0))
+        self.connect((self.lora_sdr_gray_demap_0, 0), (self.lora_sdr_modulate_0, 0))
+        self.connect((self.lora_sdr_gray_mapping_0, 0), (self.lora_sdr_deinterleaver_0, 0))
+        self.connect((self.lora_sdr_hamming_dec_0, 0), (self.lora_sdr_header_decoder_0, 0))
+        self.connect((self.lora_sdr_hamming_enc_0, 0), (self.lora_sdr_interleaver_0, 0))
+        self.connect((self.lora_sdr_header_0, 0), (self.lora_sdr_add_crc_0, 0))
+        self.connect((self.lora_sdr_header_decoder_0, 0), (self.lora_sdr_dewhitening_0, 0))
+        self.connect((self.lora_sdr_interleaver_0, 0), (self.lora_sdr_gray_demap_0, 0))
+        self.connect((self.lora_sdr_modulate_0, 0), (self.iio_pluto_sink_1, 0))
+        self.connect((self.lora_sdr_whitening_0, 0), (self.lora_sdr_header_0, 0))
+
+
+    def get_soft_decoding(self):
+        return self.soft_decoding
+
+    def set_soft_decoding(self, soft_decoding):
+        self.soft_decoding = soft_decoding
+
+    def get_sf(self):
+        return self.sf
+
+    def set_sf(self, sf):
+        self.sf = sf
+        self.lora_sdr_gray_demap_0.set_sf(self.sf)
+        self.lora_sdr_hamming_enc_0.set_sf(self.sf)
+        self.lora_sdr_interleaver_0.set_sf(self.sf)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.iio_pluto_sink_1.set_samplerate(self.samp_rate)
+        self.iio_pluto_source_0.set_samplerate(self.samp_rate)
+
+    def get_pay_len(self):
+        return self.pay_len
+
+    def set_pay_len(self, pay_len):
+        self.pay_len = pay_len
+
+    def get_impl_head(self):
+        return self.impl_head
+
+    def set_impl_head(self, impl_head):
+        self.impl_head = impl_head
+
+    def get_has_crc(self):
+        return self.has_crc
+
+    def set_has_crc(self, has_crc):
+        self.has_crc = has_crc
+
+    def get_frame_period(self):
+        return self.frame_period
+
+    def set_frame_period(self, frame_period):
+        self.frame_period = frame_period
+
+    def get_cr(self):
+        return self.cr
+
+    def set_cr(self, cr):
+        self.cr = cr
+        self.lora_sdr_hamming_enc_0.set_cr(self.cr)
+        self.lora_sdr_header_0.set_cr(self.cr)
+        self.lora_sdr_interleaver_0.set_cr(self.cr)
+
+    def get_center_freq_0(self):
+        return self.center_freq_0
+
+    def set_center_freq_0(self, center_freq_0):
+        self.center_freq_0 = center_freq_0
+        self.iio_pluto_sink_1.set_frequency(self.center_freq_0)
+
+    def get_center_freq(self):
+        return self.center_freq
+
+    def set_center_freq(self, center_freq):
+        self.center_freq = center_freq
+        self.iio_pluto_source_0.set_frequency(self.center_freq)
+
+    def get_bw(self):
+        return self.bw
+
+    def set_bw(self, bw):
+        self.bw = bw
+
+
+
+
+def main(top_block_cls=Scanner, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+    tb.flowgraph_started.set()
+
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
